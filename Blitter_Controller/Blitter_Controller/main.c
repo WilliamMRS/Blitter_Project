c88cb8f0b326d5ae40fd93d77efba9e8072b4b53
WilliamMRS <schmidtwilliammikal@gmail.com>
2022-05-05 19:27:05 +0200

    Initial commit- configured for ATmega169PA

---
+++ Blitter_Controller/Blitter_Controller/main.c
@@ -0,0 +1,22 @@
+/*
+ * Blitter_Controller.c
+ *
+ * Created: 05/04/2022 17:48:09
+ * Author : William M. R. Schmidt
+ *
+ *	ATmega169PA
+ *  ISP - JTAGICE3
+ *
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
