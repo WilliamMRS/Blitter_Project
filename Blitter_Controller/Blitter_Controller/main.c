2ad620ce263c0a921bb8f1f8ebd8e91f0bf6c825
WilliamMRS <schmidtwilliammikal@gmail.com>
2022-05-06 17:21:19 +0200

    added macros.h

--- Blitter_Controller/Blitter_Controller/main.c
+++ Blitter_Controller/Blitter_Controller/main.c
@@ -10,13 +10,27 @@
  */ 
 
 #include <avr/io.h>
-
+#include "macros.h"
 
 int main(void)
 {
-    /* Replace with your application code */
+    /* 
+		TODO:
+			Snakk med skjermen
+			Tegn noe til skjermen
+			Lag et funksjonsbibliotek for skjermen
+			
+			Få kontakt over USART
+			Brukt USART til å kjøre skjermkommandoer
+			
+			Få kontakt med flashminnet
+			Tegn fra flashminnet til skjermen
+	 */
+	
+	
     while (1) 
     {
+		
     }
 }
 
